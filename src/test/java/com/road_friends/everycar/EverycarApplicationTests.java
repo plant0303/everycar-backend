8e1e326e69236f3dea58707ae840a3a0f25d17e9
seominhee <minheee07@gmail.com>
2025-06-30 17:30:11 +0900

    first commit

---
+++ src/test/java/com/road_friends/everycar/EverycarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.road_friends.everycar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverycarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
