83c2099346450dcebac78c3c61d3c205df450d17
seominhee <minheee07@gmail.com>
2025-06-30 17:30:11 +0900

    first commit

---
+++ src/main/java/com/road_friends/everycar/EverycarApplication.java
@@ -0,0 +1,13 @@
+package com.road_friends.everycar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverycarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverycarApplication.class, args);
+	}
+
+}
