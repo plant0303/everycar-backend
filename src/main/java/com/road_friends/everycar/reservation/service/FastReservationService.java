65d07e1ffd63a1f632ceb25e6a7c92ec828b1ee2
seominhee <minheee07@gmail.com>
2025-07-10 12:45:30 +0900

    특정 지역에 있는 차량 중에서 예약되지 않은 차량만 조회

--- src/main/java/com/road_friends/everycar/reservation/service/FastReservationService.java
+++ src/main/java/com/road_friends/everycar/reservation/service/FastReservationService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.*;
 
 @Service
@@ -13,9 +15,9 @@ public class FastReservationService {
     @Autowired
     private FastReservationMapper fastReservationMapper;
 
-    public Map<String, Object> getAvailableCars(String province, String district){
+    public Map<String, Object> getAvailableCars(String province, String district, LocalDateTime rentalDatetime, LocalDateTime returnDatetime){
         // DB 검색 결과
-        List<CarDTO> availableCars = fastReservationMapper.getAvailableCars(province, district);
+        List<CarDTO> availableCars = fastReservationMapper.getAvailableCars(province, district, rentalDatetime, returnDatetime);
         // 차량정보 리스트
         // List<Map<String, Objects>> carList = new ArrayList<>();
 
