c1711c970b138552f312d6c7bf27d5f081ddc249
seominhee <minheee07@gmail.com>
2026-01-01 16:58:00 +0900

    커밋 중 오타로 인한 오류 수정

--- src/main/java/com/road_friends/everycar/user/service/APIUserService.java
+++ src/main/java/com/road_friends/everycar/user/service/APIUserService.java
@@ -66,9 +66,9 @@ public class APIUserService {
         Timestamp refreshTokenExpiredAt = new Timestamp(jwtUtil.getExpirationFromToken(refreshToken).getTime());
         APIUserMapper.updateRefreshToken(user.getUserNum(), refreshToken, refreshTokenExpiredAt);
 
-+          Map<String, String> response = new HashMap<>();
+        Map<String, String> response = new HashMap<>();
         response.put("accessToken", accessToken);
-        response.put("refreshToken", refreshToken);
+        response.put("refreshToken",refreshToken);
         response.put("userName", user.getUserName());
         response.put("userId", user.getUserId());
         return response;
