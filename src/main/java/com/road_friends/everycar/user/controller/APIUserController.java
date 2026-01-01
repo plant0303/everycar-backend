63c73afd1c8c747b315702006aaf5a3f746ba7c8
seo minhee <minheee07@gmail.com>
2026-01-01 16:40:16 +0900

    응답 결과에 username을 포함

--- src/main/java/com/road_friends/everycar/user/controller/APIUserController.java
+++ src/main/java/com/road_friends/everycar/user/controller/APIUserController.java
@@ -19,10 +19,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -33,6 +30,7 @@ import java.util.stream.Collectors;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
 public class APIUserController {
 
     private final APIUserService APIUserService;
